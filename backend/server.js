b32d491ee861737cc7471d47969312db1d56d1ea
Shehrozalam10 <shehrozalam2001@gmail.com>
2023-08-06 20:18:11 +0530

    some changes in projects

--- backend/server.js
+++ backend/server.js
@@ -30,11 +30,11 @@ app.use(express.urlencoded({extended:true}));
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-    // res.send('server is ready');
-    res.send("API is running...");
-}
-);
+// app.get('/',(req,res)=>{
+//     // res.send('server is ready');
+//     res.send("API is running...");
+// }
+// );
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
@@ -56,6 +56,23 @@ app.use('/api/config/paypal',(req,res)=>res.send({client_Id:process.env.PAYPAL_C
 const __dirname=path.resolve();
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
     
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+
+
+
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')));
+}
+else{
+    app.get('/',(req,res)=>{
+        res.send("API is running...");
+    }
+    );
+}
+
+
+
 app.use(notFound);
 app.use(errorHandler);
 
